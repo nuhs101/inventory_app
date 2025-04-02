f10fd4d735617baa64f0ffeccae95fad502bfb62
“Staxupgit config --global user.email “nuhshekhey@gmail.com <nuhshekhey@gmail.com>
2025-04-02 14:05:35 -0400

    aded inventoryapp class and added item addition and deltion

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(InventoryApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class InventoryApp extends StatelessWidget {
+  const InventoryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inventory Management',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: InventoryHomePage(),
+    );
+  }
+}
+
+class InventoryHomePage extends StatefulWidget {
+  const InventoryHomePage({super.key});
+
+  @override
+  State<InventoryHomePage> createState() => _InventoryHomePageState();
+}
+
+class _InventoryHomePageState extends State<InventoryHomePage> {
+  final TextEditingController _nameController = TextEditingController();
+  final CollectionReference _inventory = FirebaseFirestore.instance.collection(
+    'items',
+  );
+
+  void _addItem() {
+    if (_nameController.text.isNotEmpty) {
+      _inventory.add({'name': _nameController.text});
+      _nameController.clear();
+    }
+  }
+
+  void _deleteItem(String id) {
+    _inventory.doc(id).delete();
+  }
 
   // This widget is the root of your application.
   @override
